Fixing log-likelihood computation.
* Fixing a formula number of classes for RNNExtended
* Fixing log-likelihood formula computation for RNN
* Computing log-likelihood for RNNExtended

src/rnn.py
@@ -27,7 +27,7 @@ class RNN:
         s_t = sigmoid(self.U.dot(x) + self.W.dot(self.s[1]))
         return softmax(self.V.dot(self.s[0]))
 
-    def sentence_log_likelihood(self, Xi):
+    def _sentence_log_likelihood(self, Xi):
         X = np.zeros((len(Xi), self.N))
         for idx, xi in enumerate(Xi):
             X[idx][xi] = 1
@@ -35,17 +35,17 @@ class RNN:
         h = X[:-1].dot(self.U.T) + self.s[1].dot(self.W)
         log_q = h.dot(self.V.T)
         a = np.max(log_q, axis=1)
-        log_Z = a + np.log(np.sum(np.exp((log_q.T - a).T)))
+        log_Z = a + np.log(np.sum(np.exp((log_q.T - a).T), axis=1))
         #print log_Z
         return np.sum(np.array([log_q[index, value]
-                                for (index,), value in np.ndenumerate(Xi[1:])])
+                                for index, value in enumerate(Xi[1:])])
                       - log_Z)
 
     def log_likelihood(self, Xii):
         """
             Xii is a list of list of indexes. Each list represent separate sentence
         """
-        return sum([self.sentence_log_likelihood(Xi) for Xi in Xii])
+        return sum([self._sentence_log_likelihood(Xi) for Xi in Xii])
 
     def train(self, Xi, lr=0.1):
         for xi, di in zip(Xi, Xi[1:]):

src/rnn_extended.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
+import rnn
 from rnn_routine import *
 
 class RNNExtended:
@@ -14,7 +15,8 @@ class RNNExtended:
         self.U = np.random.randn(self.H, self.N)
         self.W = np.random.randn(self.H, self.H)
         self.V = np.random.randn(self.class_size, self.H)
-        nclass = np.ceil(self.N / self.class_size)
+        nclass = np.ceil(float(self.N) / self.class_size)
+        print("Number of classes: %d" % (nclass))
         self.X = np.random.randn(nclass, self.H)
 
         # Initial state of the hidden layer
@@ -31,6 +33,43 @@ class RNNExtended:
     def predict(self, x):
         s_t = sigmoid(self.U.dot(x) + self.W.dot(self.s[1]))
         return softmax(self.V.dot(self.s[0]))
+
+    def _sentence_log_likelihood(self, Xi):
+        h, index_lik = super(RNN, self)._sentence_log_likelihood(self, Xi)
+        log_c = h.dot(self.X.T)
+        a = np.max(log_c, axis=1)
+        log_Z = a + np.log(np.sum(np.exp((log_c.T - a).T), axis=1))
+        return h, np.sum(np.array([log_c[index, value]
+                                   for index, value in enumerate(Xi[1:])])
+                         - log_Z)
+
+    def _sentence_log_likelihood(self, Xi):
+        X = np.zeros((len(Xi), self.N))
+        for idx, xi in enumerate(Xi):
+            X[idx][xi] = 1
+
+        h = X[:-1].dot(self.U.T) + self.s[1].dot(self.W)
+        log_q = h.dot(self.V.T)
+        a = np.max(log_q, axis=1)
+        log_Z = a + np.log(np.sum(np.exp((log_q.T - a).T), axis=1))
+
+        log_c = h.dot(self.X.T)
+        a = np.max(log_c, axis=1)
+        log_C = a + np.log(np.sum(np.exp((log_c.T - a).T), axis=1))
+
+        #print log_Z
+        return np.sum(np.array([log_q[index, value % self.class_size]
+                                for index, value in enumerate(Xi[1:])])
+                      - log_Z) +\
+               np.sum(np.array([log_c[index, value // self.class_size]
+                                for index, value in enumerate(Xi[1:])])
+                      - log_C)
+
+    def log_likelihood(self, Xii):
+        """
+            Xii is a list of list of indexes. Each list represent separate sentence
+        """
+        return sum([self._sentence_log_likelihood(Xi) for Xi in Xii])
 
     def train(self, Xi, lr=0.1):
         for xi, di in zip(Xi, Xi[1:]):